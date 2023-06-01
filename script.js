6a87ba0c6563b09194d927b0225f19882ad49121
[akhil20015] <[akhilsaras2001@gmail.com]>
2023-06-01 10:16:01 +0530

    js changes

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const nav = document.getElementById('navbar');
-const bar = document.getElementById('bar')
 
-// const verNavbar = ()=>{
-//     nav.classList.add('active');
-// }
-if(bar){
-    bar.addEventListener('click', ()=>{
-        nav.classList.add('active');
-    })
+const verNavbar = ()=>{
+    nav.classList.add('active');
 }
-else{
+
+const closeNavbar = ()=>{
     nav.classList.remove('active');
 }
+  
