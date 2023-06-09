56eab767ef4dea10ad5fc2202cc0fb3eec98b9ca
[akhil20015] <[akhilsaras2001@gmail.com]>
2023-06-09 18:26:41 +0530

    shooping cart

--- script.js
+++ script.js
@@ -8,14 +8,27 @@ console.log(products);
 
 // NAVBAR HAMBURGER MENU
 const nav = document.getElementById('navbar');
-const verNavbar = ()=>{
-    nav.classList.toggle('active');
-}
+const hamBur = document.getElementById('bar');
+const close = document.getElementById('close');
 
-const closeNavbar = ()=>{
+hamBur.addEventListener('click', ()=>{
+    nav.classList.toggle('active');
+});
+
+close.addEventListener('click', ()=>{
     nav.classList.remove('active');
-}
+});
+
+//  FUNCTION NOT BEING CALLED IN ONCLICK ATTRIBUTE
+// const verNavbar = ()=>{
+
+// }
+
+// const closeNavbar = ()=>{
+// }
   
+
+
 
 // let main = document.getElementById('main-img');
 
@@ -25,48 +38,127 @@ const closeNavbar = ()=>{
 
 function Productcards() {
     const card = document.getElementById('pro-cont');
-    for(let item in data){
-
+    data.forEach( val =>{
         let cardElement = `
         <div class="pro">
-            <img src="${data[item].imageUrl}" alt="">
+            <img src="${val.imageUrl}" alt="">
             <div class="des">
-                <span>${data[item].brand}</span>
-                <h5>${data[item].name}</h5>
+                <span>${val.brand}</span>
+                <h5>${val.name}</h5>
                 <div class="star">
-                    <i class="fas fa-star ${data[item].ratings >= 1 ? 'fa-gold' : 'fa-gray'}"></i>
-                    <i class="fas fa-star ${data[item].ratings >= 2 ? 'fa-gold' : 'fa-gray'}"></i>
-                    <i class="fas fa-star ${data[item].ratings >= 3 ? 'fa-gold' : 'fa-gray'}"></i>
-                    <i class="fas fa-star ${data[item].ratings >= 4 ? 'fa-gold' : 'fa-gray'}"></i>
-                    <i class="fas fa-star ${data[item].ratings == 5 ? 'fa-gold' : 'fa-gray'}"></i>
+                    <i class="fas fa-star ${val.ratings >= 1 ? 'fa-gold' : 'fa-gray'}"></i>
+                    <i class="fas fa-star ${val.ratings >= 2 ? 'fa-gold' : 'fa-gray'}"></i>
+                    <i class="fas fa-star ${val.ratings >= 3 ? 'fa-gold' : 'fa-gray'}"></i>
+                    <i class="fas fa-star ${val.ratings >= 4 ? 'fa-gold' : 'fa-gray'}"></i>
+                    <i class="fas fa-star ${val.ratings == 5 ? 'fa-gold' : 'fa-gray'}"></i>
                 </div>
-            <h4>${data[item].price}</h4>
+            <h4>${val.price}</h4>
             </div>
-            <a><i id="${data[item].id}" class="fa-sharp fa-solid fa-cart-shopping cart"></i></a>
+            <a><i id="${val.id}" class="fa-sharp fa-solid fa-cart-shopping cart"></i></a>
         </div>`;
         card.innerHTML += cardElement;
-    }
+    });
 }
 Productcards();
 
 
-const badge = document.getElementById('cart-badge');
-let quantity = [];
-data.forEach(e =>{
-    const shopBtn = document.getElementById(e.id);
-    shopBtn.addEventListener('click', ele =>{
-        quantity.push(ele.target.id);
-        let count=quantity.length;
-        badge.innerText = count;
-        console.log(quantity);
-        // sessionStorage.setItem("counter",quantity);
+const cart = ()=> {
+    const badge = document.getElementById('cart-badge');
+    let quantity = [];
+    let count=0;
+    data.forEach(e =>{
+        const shopBtn = document.getElementById(e.id);
+        shopBtn.addEventListener('click', ele =>{
+                quantity.push(ele.target.id);
+                count=quantity.length;
+                badge.innerText = count;
+                sessionStorage.setItem("counter",JSON.stringify(quantity));  
+                // badge.style.display = "flex";
+                // console.log(quantity); 
+        });
+        // badge.style.display = "none";
     });
+    // let valu = sessionStorage.getItem("counter");
+    // let proIds = JSON.parse(valu);
+    // // return proIds;
+    // displayCart(proIds);
+};cart();
+
+const shpCart = document.getElementById('shop');
+const checkout = document.getElementById('shoppingCart');
+
+shpCart.addEventListener('click',()=>{
+    let items = sessionStorage.getItem("products");
+    let x = JSON.parse(items);
+
+    let val = sessionStorage.getItem("counter");
+    let proIds = JSON.parse(val);
+    let arr = Object.values(proIds);
+
+    // let redund = arr.filter(arr =>{
+        
+    //     return arr;
+    // });
+    // console.log(redund);
+   
+    let unique = [];
+    arr.forEach((id,key)=>{
+        // console.log(key);
+        let qty=0;
+        if(!unique.includes(id)){
+            unique.push(id);
+            qty++;
+        }
+        else{
+            qty++;
+        }
+       x.forEach(prod =>{
+        console.log(typeof unique[0]);
+        let uniquenum = unique.map(Number);
+        // if(prod.id == id)
+            if(prod.id == uniquenum[key])
+            {
+                let cartItems = `
+                <div class="prodDetails">
+                        <img src="${prod.imageUrl}" alt="">
+                        <div class="des">
+                            <span>${prod.brand}</span>
+                            <h5>${prod.name}</h5>
+                            <div class="star">
+                                <i class="fas fa-star ${prod.ratings >= 1 ? 'fa-gold' : 'fa-gray'}"></i>
+                                <i class="fas fa-star ${prod.ratings >= 2 ? 'fa-gold' : 'fa-gray'}"></i>
+                                <i class="fas fa-star ${prod.ratings >= 3 ? 'fa-gold' : 'fa-gray'}"></i>
+                                <i class="fas fa-star ${prod.ratings >= 4 ? 'fa-gold' : 'fa-gray'}"></i>
+                                <i class="fas fa-star ${prod.ratings == 5 ? 'fa-gold' : 'fa-gray'}"></i>
+                            </div>
+                            <h4>${prod.price}</h4>
+                        </div>
+                        <div class="func"> 
+                            <h5>${qty}</h5>
+                            <h3>Remove</h3>
+                            <h3>Buy</h3>
+                        </div>
+                    </div>`;
+                    checkout.innerHTML += cartItems;
+            }
+       });
+    });
+    document.getElementsByTagName('body')[0].style.overflow = "hidden";
+    document.getElementsByClassName('overlay')[0].style.display = "block";
+    document.getElementById('blur').style.display="block";
+
+    // console.log(products);
 });
+
+
+
 
 let shoppingCart = sessionStorage.getItem("counter");
 // console.log(shoppingCart);
 // sessionStorage.removeItem("quantites")
 // sessionStorage.setItem("quantites",shoppingCart);
+
+
 
 // console.log(shopBtn);
 // shopBtn.addEventListener('click', (e)=>{
